5c9ba27dd28c026d2ba2ee824e0ec2babd1cc6a7
mathiasomar <ommykmathy@gmail.com>
2024-12-16 21:43:12 +0300

    Tailwind config fontsize and font family

--- user/tailwind.config.js
+++ user/tailwind.config.js
@@ -9,7 +9,27 @@ export default {
       colors: {
         background: "#000",
         backgroundContrast: "#111",
-        textBlack: "1d1d1f"
+        textBlack: "#1d1d1f"
+      },
+      fontFamily: {
+        sans: [
+          "SF Pro Display",
+          "Helvetica Neue",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+      fontSize: {
+        xs: "0.75rem", //12px
+        sm: "0.875rem", // 14px
+        base: "1.0625rem", //17px
+        lg: "1.1875rem", // 19px
+        xl: "1.3125rem", // 21px
+        "2xl": "1.5rem", // 24px
+        "3xl": "1.75rem", // 28px
+        "4xl": "2.5rem", // 40px
+        "5xl": "4.5rem", // 72px
       }
     },
   },
